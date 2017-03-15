3c0e2e46fec2d8211d490254c84cc74144226778
Jayanth Shetty <jayanthshetty@jayanth.local>
2017-03-15 13:27:24 +0530

    Initial Commit

---
+++ GitLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitLearning
+//
+//  Created by Jayanth Shetty on 2017-03-15.
+//  Copyright © 2017 Sprouts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
